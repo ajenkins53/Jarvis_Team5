18e06095fe309ed1eb314885850743d4af8d8e86
spatel1010 <spatel69@luc.edu>
2017-03-29 18:35:00 -0500

    Test module for Pickuplines
    test module taken from original JARVIS modules, form-fitted to pick up lines

---
+++ modules/test_pickuplines.py
@@ -0,0 +1,7 @@
+import modules
+
+def test_pickuplines():
+    assert('pick up line' == modules.process_query('tell me a pick up line')[0])
+    assert('pick up line' == modules.process_query('Do you know a pick up line?')[0])
+    assert('pick up line' == modules.process_query('random pick up lines')[0])
+    assert('pick up line' != modules.process_query('something random')[0])
