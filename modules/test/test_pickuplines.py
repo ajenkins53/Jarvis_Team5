76d37b0ff811fb0cf24ed8c06c34a36515dab8e7
spatel1010 <spatel69@luc.edu>
2017-04-05 19:16:56 -0500

    Update test_pickuplines.py

--- modules/test/test_pickuplines.py
+++ modules/test/test_pickuplines.py
@@ -5,7 +5,7 @@ def test_pickuplines():
     assert('pick up line' == modules.process_query('pick up line')[0])
     assert('pick up line' == modules.process_query('pickup line')[0])
     assert('pick up line' == modules.process_query('give me a pickup line')[0])
-    assert('pick up line' == modules.process_query('give me a pickup line')[0])
+    assert('pick up line' == modules.process_query('give me a pick up line')[0])
     assert('pick up line' == modules.process_query('Do you know a pick up line?')[0])
     assert('pick up line' == modules.process_query('random pick up lines')[0])
     assert('pick up line' != modules.process_query('something random')[0])
