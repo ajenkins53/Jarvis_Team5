99a87e90edf15fd20aa1812ab50ce75640aadec7
spatel1010 <spatel69@luc.edu>
2017-04-05 19:16:38 -0500

    More asserts
    Added more common ways to ask for a pickup line

--- modules/test/test_pickuplines.py
+++ modules/test/test_pickuplines.py
@@ -2,6 +2,10 @@ import modules
 
 def test_pickuplines():
     assert('pick up line' == modules.process_query('tell me a pick up line')[0])
+    assert('pick up line' == modules.process_query('pick up line')[0])
+    assert('pick up line' == modules.process_query('pickup line')[0])
+    assert('pick up line' == modules.process_query('give me a pickup line')[0])
+    assert('pick up line' == modules.process_query('give me a pickup line')[0])
     assert('pick up line' == modules.process_query('Do you know a pick up line?')[0])
     assert('pick up line' == modules.process_query('random pick up lines')[0])
     assert('pick up line' != modules.process_query('something random')[0])
