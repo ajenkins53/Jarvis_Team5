3b72b7e4731b2f366914b438611e6a0a7e4a1636
ajenkins53 <ajenkins@luc.edu>
2017-04-05 19:27:55 -0500

    Delete test_riddle.py

--- modules/test_riddle.py
+++
@@ -1,7 +0,0 @@
-import modules
-
-def test_riddle():
-    assert('riddle' == modules.process_query('tell me a riddle')[0])
-    assert('riddle' == modules.process_query('Do you know a riddle?')[0])
-    assert('riddle' == modules.process_query('random riddle')[0])
-    
